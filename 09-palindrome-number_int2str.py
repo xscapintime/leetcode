3e8c904dd2ef936826d2ca8549c1ce4c06c77662
xscapintime <louiesxscape@gmail.com>
2022-02-23 20:44:11 +0800

    leetcode 09

--- 09-palindrome-number_int2str.py
+++ 09-palindrome-number_int2str.py
@@ -1,3 +1,16 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return(str(x) == str(x)[::-1])
+
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return(False)
+        else:
+            if len(str(x))%2 == 0:
+                return(str(x)[0:len(str(x))//2] == str(x)[len(str(x))//2:len(str(x))][::-1])
+            else:
+                return(str(x)[0:len(str(x))//2+1] == str(x)[len(str(x))//2:len(str(x))][::-1])
+
